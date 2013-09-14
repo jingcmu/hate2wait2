c98a37bc92852647eac7f40863ceb6a9c3e92e5a
Jacky <jingcmu@qq.com>
2013-09-14 16:50:41 -0700

    Initial commit

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
+# Initialize the rails application
+Hate2wait2::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hate2wait2::Application.config.secret_token = 'b92e83fdd7ef772f6ad1a3d1fe2380114e7ed019431b17607781c04dd8a1631f6d0f6b860976f91375e038f67c9ff2ec1bf6d7bf0d5e6521f787d39da568705d'
