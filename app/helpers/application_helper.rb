11c975904598c9ca618d735a48dd48aa85dbacdd
Jacky <jingcmu@qq.com>
2013-09-15 23:17:45 -0700

    Added UI and will submit in one hour

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  #based on the title for each page, return the full title
+  def full_title(page_title)
+    base_title = "Hate2wait"
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
+
 end
