abf91b68e952cf1a202e9a83e0e391b726b878a4
Jacky <jingcmu@qq.com>
2013-09-17 23:36:30 -0700

    finish Restful API and some other stuff

--- app/helpers/locations_helper.rb
+++ app/helpers/locations_helper.rb
@@ -7,10 +7,12 @@ module LocationsHelper
     #url = URI.parse('http://localhost:3000/seats')
     url = URI.parse('http://hate2wait2.herokuapp.com/seats')
     xml_data = Net::HTTP.get_response(url).body
-=begin
+
     hash_data = Hash.from_xml(xml_data)
     json_data = hash_data.to_json
     number_of_seats = 0
+    json_data
+=begin
     seats = hash_data["seats"]
     seats.each do |seat|
       seat.each do |key, value|
