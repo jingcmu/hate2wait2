a7ab66da80cc9dd5e8d5b128c6ebf3b059fb86e0
Jacky <jingcmu@qq.com>
2013-09-18 00:40:25 -0700

    finish Restful API and some other stuff

--- app/helpers/locations_helper.rb
+++ app/helpers/locations_helper.rb
@@ -3,17 +3,20 @@ require 'net/http'
 require 'rexml/document'
 
 module LocationsHelper
-  def get_seats_number(location_id)
-    #url = URI.parse('http://localhost:3000/seats')
-    url = URI.parse('http://hate2wait.herokuapp.com/seats')
-    xml_data = Net::HTTP.get_response(url).body
 
+  def get_seats_number(location_id)
+    #url = URI.parse('localhost','/seats',3000)
+    #url = URI.parse('http://hate2wait.herokuapp.com/seats')
+    xml_data = Net::HTTP.get_response('http://hate2wait.herokuapp.com','/seats',80).body
+    xml_data
+=begin
     hash_data = Hash.from_xml(xml_data)
     json_data = hash_data.to_json
     number_of_seats = 0
-    json_data
-=begin
     seats = hash_data["seats"]
+    if seats == nil
+      return number_of_seats
+    end
     seats.each do |seat|
       seat.each do |key, value|
         if key.to_s == 'location_id' and value == location_id
@@ -21,8 +24,9 @@ module LocationsHelper
         end
       end
     end
-
     number_of_seats
 =end
+    #xml_data
   end
+
 end
