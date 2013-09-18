21d91cea43ba3847a253da76494c283a45ba1e70
Jacky <jingcmu@qq.com>
2013-09-18 01:04:18 -0700

    finish Restful API and some other stuff

--- app/helpers/locations_helper.rb
+++ app/helpers/locations_helper.rb
@@ -1,14 +1,16 @@
 require 'json'
 require 'net/http'
 require 'rexml/document'
+require 'uri'
 
 module LocationsHelper
 
   def get_seats_number(location_id)
     #url = URI.parse('localhost','/seats',3000)
-    #url = URI.parse('http://hate2wait.herokuapp.com/seats')
-    xml_data = Net::HTTP.get_response('http://hate2wait.herokuapp.com','/seats',80).body
-    xml_data
+    url = URI.parse('http://hate2wait.herokuapp.com/seats/new')
+    #xml_data = Net::HTTP.get_response('http://hate2wait2.herokuapp.com','/seats').body
+    Net::HTTP.post_form (url, "name" => "xiaoli", "location_id" => "2")
+    #xml_data
 =begin
     hash_data = Hash.from_xml(xml_data)
     json_data = hash_data.to_json
